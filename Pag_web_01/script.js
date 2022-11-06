89835a86c643c70505e79027037b9e8d3814f39d
Nestor Rios <Np2Rs8@gmail.com>
2022-11-06 01:54:56 -0500

    Tabla 2
    Se añadió el primer borrador de prueba de tabla funcional

--- Pag_web_01/script.js
+++ Pag_web_01/script.js
@@ -7,3 +7,24 @@ function saludo() {
 
     alert(`Hola ${nombre}`);
 }
+
+
+function texto() {
+    let campText = document.querySelector('#campTxt');
+    
+    if((campText.value).length === 3){
+        campText.value = (campText.value - 1) + '\n';
+    }
+    
+}
+
+function texto2() {
+    let textArea = document.querySelector('#areaTexto');
+
+
+
+    if((textArea.value).length === 88){
+        alert((textArea.value).length);
+    }
+    
+}
